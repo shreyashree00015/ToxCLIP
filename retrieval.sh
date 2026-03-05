89d68ab1800e2381372db4b334ae6b678013c9b4
j53meng <j53meng@uwaterloo.ca>
2026-03-05 13:23:20 -0500

    modify retrival

--- retrieval.sh
+++ retrieval.sh
@@ -5,7 +5,7 @@ MOL_PATH="mols.lmdb" # path to the molecule file
 POCKET_PATH="pocket.lmdb" # path to the pocket file
 EMB_DIR="./data/emb" # path to the cached mol embedding file
 
-CUDA_VISIBLE_DEVICES="1" python ./unimol/retrieval.py --user-dir ./unimol $data_path "./data" --valid-subset test \
+CUDA_VISIBLE_DEVICES="0" python ./unimol/retrieval.py --user-dir ./unimol $data_path "./data" --valid-subset test \
        --results-path $results_path \
        --num-workers 8 --ddp-backend=c10d --batch-size $batch_size \
        --task drugclip --loss in_batch_softmax --arch drugclip  \
